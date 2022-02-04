af7abdcd7667ca8c600f51762a8dd717c2243425
Jonas Windhager <jonas.windhager@uzh.ch>
2022-02-04 10:43:26 +0100

    Fix contrast_limits for uint16 (closes #5)

--- napari_czifile2/_reader.py
+++ napari_czifile2/_reader.py
@@ -10,10 +10,10 @@ from napari_czifile2.io import CZISceneFile
 @napari_hook_implementation
 def napari_get_reader(path):
     if isinstance(path, list):
-        if any(Path(p).suffix.lower() != '.czi' for p in path):
+        if any(Path(p).suffix.lower() != ".czi" for p in path):
             return None
     else:
-        if Path(path).suffix.lower() != '.czi':
+        if Path(path).suffix.lower() != ".czi":
             return None
     return reader_function
 
@@ -27,20 +27,32 @@ def reader_function(paths):
         for scene_index in range(num_scenes):
             with CZISceneFile(path, scene_index) as f:
                 data = f.as_tzcyx0_array(max_workers=cpu_count())
+                # https://github.com/BodenmillerGroup/napari-czifile2/issues/5
+                contrast_limits = None
+                if data.dtype == np.uint16:
+                    contrast_limits = (0, 65535)
                 # https://github.com/napari/napari/issues/2348
-                # https://github.com/BodenmillerGroup/napari-czifile2/issues/4
                 if not f.is_rgb:
                     data = data[:, :, :, :, :, 0]
                 metadata = {
-                    'rgb': f.is_rgb,
-                    'channel_axis': 2,
-                    'translate': (f.pos_t_seconds, f.pos_z_um, f.pos_y_um, f.pos_x_um),
-                    'scale': (f.scale_t_seconds, f.scale_z_um, f.scale_y_um, f.scale_x_um),
+                    "rgb": f.is_rgb,
+                    "channel_axis": 2,
+                    "translate": (f.pos_t_seconds, f.pos_z_um, f.pos_y_um, f.pos_x_um),
+                    "scale": (
+                        f.scale_t_seconds,
+                        f.scale_z_um,
+                        f.scale_y_um,
+                        f.scale_x_um,
+                    ),
+                    "contrast_limits": contrast_limits,
                 }
                 if f.channel_names is not None:
                     if num_scenes == 1:
-                        metadata['name'] = f.channel_names
+                        metadata["name"] = f.channel_names
                     elif num_scenes > 1:
-                        metadata['name'] = [f'S{scene_index:02d} {channel_name}' for channel_name in f.channel_names]
-            layer_data.append((data, metadata, 'image'))
+                        metadata["name"] = [
+                            f"S{scene_index:02d} {channel_name}"
+                            for channel_name in f.channel_names
+                        ]
+            layer_data.append((data, metadata, "image"))
     return layer_data
--- napari_czifile2/io.py
+++ napari_czifile2/io.py
@@ -11,8 +11,8 @@ class CZISceneFile(CziFile):
     @staticmethod
     def get_num_scenes(path: Union[str, Path], *args, **kwargs) -> int:
         with CziFile(path, *args, **kwargs) as czi_file:
-            if 'S' in czi_file.axes:
-                return czi_file.shape[czi_file.axes.index('S')]
+            if "S" in czi_file.axes:
+                return czi_file.shape[czi_file.axes.index("S")]
             return 1
 
     def __init__(self, path: Union[str, Path], scene_index: int, *args, **kwargs):
@@ -21,70 +21,80 @@ class CZISceneFile(CziFile):
 
     @lazyattr
     def pos_x_um(self) -> float:
-        return min((dim_entry.start for dim_entry in self._iter_dim_entries('X')), default=0.) * self.scale_x_um
+        return self.scale_x_um * min(
+            (dim_entry.start for dim_entry in self._iter_dim_entries("X")), default=0.0
+        )
 
     @lazyattr
     def pos_y_um(self) -> float:
-        return min((dim_entry.start for dim_entry in self._iter_dim_entries('Y')), default=0.) * self.scale_y_um
+        return self.scale_y_um * min(
+            (dim_entry.start for dim_entry in self._iter_dim_entries("Y")), default=0.0
+        )
 
     @lazyattr
     def pos_z_um(self) -> float:
-        return min((dim_entry.start for dim_entry in self._iter_dim_entries('Z')), default=0.) * self.scale_z_um
+        return self.scale_z_um * min(
+            (dim_entry.start for dim_entry in self._iter_dim_entries("Z")), default=0.0
+        )
 
     @lazyattr
     def pos_t_seconds(self) -> float:
-        return min((dim_entry.start for dim_entry in self._iter_dim_entries('T')), default=0.) * self.scale_t_seconds
+        return self.scale_t_seconds * min(
+            (dim_entry.start for dim_entry in self._iter_dim_entries("T")), default=0.0
+        )
 
     @lazyattr
     def scale_x_um(self) -> float:
-        return self._get_scale('X', multiplier=10. ** 6)
+        return self._get_scale("X", multiplier=10.0**6)
 
     @lazyattr
     def scale_y_um(self) -> float:
-        return self._get_scale('Y', multiplier=10. ** 6)
+        return self._get_scale("Y", multiplier=10.0**6)
 
     @lazyattr
     def scale_z_um(self) -> float:
-        return self._get_scale('Z', multiplier=10. ** 6)
+        return self._get_scale("Z", multiplier=10.0**6)
 
     @lazyattr
     def scale_t_seconds(self) -> float:
-        return self._get_scale('T')
+        return self._get_scale("T")
 
     @lazyattr
     def channel_names(self) -> Optional[List[str]]:
-        if 'C' in self.axes:
-            channel_elements = self._metadata_xml.findall('.//Metadata/Information/Image/Dimensions/Channels/Channel')
-            if len(channel_elements) == self.shape[self.axes.index('C')]:
-                return [c.attrib.get('Name', c.attrib['Id']) for c in channel_elements]
+        if "C" in self.axes:
+            channel_elements = self._metadata_xml.findall(
+                ".//Metadata/Information/Image/Dimensions/Channels/Channel"
+            )
+            if len(channel_elements) == self.shape[self.axes.index("C")]:
+                return [c.attrib.get("Name", c.attrib["Id"]) for c in channel_elements]
         return None
 
     @lazyattr
     def is_rgb(self) -> bool:
-        return '0' in self.axes and self.shape[self.axes.index('0')] > 1
+        return "0" in self.axes and self.shape[self.axes.index("0")] > 1
 
     def as_tzcyx0_array(self, *args, **kwargs) -> np.ndarray:
         data = self.asarray(*args, **kwargs)
         tzcyx0_axis_indices = []
-        if 'T' in self.axes:
-            tzcyx0_axis_indices.append(self.axes.index('T'))
+        if "T" in self.axes:
+            tzcyx0_axis_indices.append(self.axes.index("T"))
         else:
             tzcyx0_axis_indices.append(data.ndim)
             data = np.expand_dims(data, -1)
-        if 'Z' in self.axes:
-            tzcyx0_axis_indices.append(self.axes.index('Z'))
+        if "Z" in self.axes:
+            tzcyx0_axis_indices.append(self.axes.index("Z"))
         else:
             tzcyx0_axis_indices.append(data.ndim)
             data = np.expand_dims(data, -1)
-        if 'C' in self.axes:
-            tzcyx0_axis_indices.append(self.axes.index('C'))
+        if "C" in self.axes:
+            tzcyx0_axis_indices.append(self.axes.index("C"))
         else:
             tzcyx0_axis_indices.append(data.ndim)
             data = np.expand_dims(data, -1)
-        tzcyx0_axis_indices.append(self.axes.index('Y'))
-        tzcyx0_axis_indices.append(self.axes.index('X'))
-        if '0' in self.axes:
-            tzcyx0_axis_indices.append(self.axes.index('0'))
+        tzcyx0_axis_indices.append(self.axes.index("Y"))
+        tzcyx0_axis_indices.append(self.axes.index("X"))
+        if "0" in self.axes:
+            tzcyx0_axis_indices.append(self.axes.index("0"))
         else:
             tzcyx0_axis_indices.append(data.ndim)
             data = np.expand_dims(data, -1)
@@ -101,13 +111,15 @@ class CZISceneFile(CziFile):
                 if dim_entry.dimension == dimension:
                     yield dim_entry
 
-    def _get_scale(self, dimension: str, multiplier: float = 1.):
-        scale_element = self._metadata_xml.find(f'.//Metadata/Scaling/Items/Distance[@Id="{dimension}"]/Value')
+    def _get_scale(self, dimension: str, multiplier: float = 1.0):
+        scale_element = self._metadata_xml.find(
+            f'.//Metadata/Scaling/Items/Distance[@Id="{dimension}"]/Value'
+        )
         if scale_element is not None:
             scale = float(scale_element.text)
             if scale > 0:
                 return scale * multiplier
-        return 1.
+        return 1.0
 
     @lazyattr
     def _metadata_xml(self) -> ElementTree.Element:
@@ -116,11 +128,20 @@ class CZISceneFile(CziFile):
     @lazyattr
     def filtered_subblock_directory(self) -> List[DirectoryEntryDV]:
         dir_entries = super(CZISceneFile, self).filtered_subblock_directory
-        return list(filter(lambda dir_entry: self._get_scene_index(dir_entry) == self.scene_index, dir_entries))
+        return list(
+            filter(
+                lambda dir_entry: self._get_scene_index(dir_entry) == self.scene_index,
+                dir_entries,
+            )
+        )
 
     @staticmethod
     def _get_scene_index(dir_entry: DirectoryEntryDV) -> int:
-        scene_indices = {dim_entry.start for dim_entry in dir_entry.dimension_entries if dim_entry.dimension == 'S'}
+        scene_indices = {
+            dim_entry.start
+            for dim_entry in dir_entry.dimension_entries
+            if dim_entry.dimension == "S"
+        }
         if len(scene_indices) == 0:
             return 0
         assert len(scene_indices) == 1
