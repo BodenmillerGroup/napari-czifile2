564c1220dd38dc65ec886c1397fdc0776c9eefd8
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2025-03-14 16:51:46 -0400

    use try/except to import lazyattr

--- napari_czifile2/io.py
+++ napari_czifile2/io.py
@@ -4,7 +4,10 @@ from xml.etree import ElementTree
 
 import numpy as np
 from czifile import CziFile, DimensionEntryDV1, DirectoryEntryDV
-from tifffile import lazyattr
+try:
+    from tifffile import lazyattr
+except ImportError:
+    from czifile.czifile import lazyattr
 
 
 class CZISceneFile(CziFile):
